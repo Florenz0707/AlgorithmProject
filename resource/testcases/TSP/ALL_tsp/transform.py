090b3649e39ab24eb5fe98ba05236fc2d334a9e3
Florenz <florenz@tju.edu.cn>
2025-05-26 11:15:08 +0800

    Still debuging.

--- resource/testcases/TSP/ALL_tsp/transform.py
+++ resource/testcases/TSP/ALL_tsp/transform.py
@@ -7,8 +7,11 @@ def name_kind(filename: str) -> list:
 
 if __name__ == "__main__":
     cwd = r"D:\_Files\_Lessons\ALGORITHM\Project\resource\testcases\TSP\ALL_tsp"
+    os.chdir(cwd)
     files = os.listdir(cwd)
+    generate = False
     for file in files:
+        print(file)
         splits = name_kind(file)
         if splits[1] == "tsp":
             n = 20
@@ -18,22 +21,17 @@ if __name__ == "__main__":
                 while line := f.readline().strip():
                     if line == "EOF":
                         break
-                    print(line)
                     if line.startswith("DIMENSION"):
                         n = int(line.replace(" ", "").split(":")[1])
-                    if line == "NODE_COORD_SECTION":
+                    if line == "NODE_COORD_SECTION" or line == "DISPLAY_DATA_SECTION":
                         begin = True
                         continue
                     if begin:
                         info = line.split(" ")
-                        i = 0
-                        while i < len(info):
-                            if info[i] == "":
-                                info.remove(info[i])
-                            else:
-                                i += 1
+                        while "" in info:
+                            info.remove("")
                         print(info)
-                        nodes.append((int(info[1]), int(info[2])))
+                        nodes.append((int(eval(info[1])), int(eval(info[2]))))
 
             distance = [[0 for i in range(n)] for j in range(n)]
             for i in range(n):
@@ -41,11 +39,12 @@ if __name__ == "__main__":
                     distance[i][j] = int(((nodes[i][0] - nodes[j][0]) ** 2 + (nodes[i][1] - nodes[j][1]) ** 2) ** 0.5)
                     distance[j][i] = distance[i][j]
 
-            with open(f"{splits[0]}.txt", 'w') as f:
-                f.write(str(n) + "\n")
-                for i in range(n):
-                    for j in range(n):
-                        f.write(str(distance[i][j]) + " ")
-                    f.write("\n")
+            if generate:
+                with open(f"{splits[0]}.txt", 'w') as f:
+                    f.write(str(n) + "\n")
+                    for i in range(n):
+                        for j in range(n):
+                            f.write(str(distance[i][j]) + " ")
+                        f.write("\n")
 
 
