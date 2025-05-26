585415e4ee8526a0e12cb33151f20d8659217101
Florenz <florenz@tju.edu.cn>
2025-05-26 22:04:47 +0800

    Exclude dir<result> and modified <script.sh> to make it more meaningful.

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ find "$CODE_DIR" -type f | while IFS= read -r src;
 do
 	src_name="${src##*/}"
 	out_name="${src_name%%.*}.out"
-	echo "$src_name"
+	echo "ready to compile $src_name and run..."
 	g++ "$src" -o "$out_name"
 
 	find "$DATA_DIR" -type f | while IFS= read -r file;
@@ -25,11 +25,8 @@ do
     	ans="-ans.txt"
     	if [[ $file_name == *$txt* ]] && ! [[ $file_name == *$ans* ]]
     	then
-    		echo "$file_name"
+    		echo "testing $file_name"
     		"./$out_name" < "$file" > "./result/${src_name%%.*}-${file_name%%.*}.txt"
     	fi
     done
 done
-
-
-
